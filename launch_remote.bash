2f767a580e31f7d07d41769ea722557355d2411e
Antonio Ragagnin <ragagnin@dorc1.usm.uni-muenchen.de>
2018-06-22 13:57:59 +0200

    fix

--- launch_remote.bash
+++ launch_remote.bash
@@ -20,7 +20,7 @@ rsync -v --checksum $SQL $SSH_HOST:$F/$TOSQL
 ssh $SSH_HOST $INTRO '&&' cd $F '&&' find
 OUTPUT=output/$(echo $NAME|sed s,/,_,g)_$PROP
 echo ssh $SSH_HOST $INTRO '&&' cd $F '&&' DB=$TOSQL $PYTHON composite_properties.py --basegroup $BASE/groups_$SNAP/sub_$SNAP --basesnap $BASE/snapdir_$SNAP/snap_$SNAP --simulation-name $NAME   --snap $SNAP --chunk 0 --chunks 10  --prop $PROP '>' $OUTPUT
-ssh $SSH_HOST $INTRO '&&' cd $F '&&' DB=$TOSQL $PYTHON composite_properties.py --basegroup $BASE/groups_$SNAP/sub_$SNAP --basesnap $BASE/snapdir_$SNAP/snap_$SNAP --simulation-name $NAME   --snap $SNAP --chunk 0 --chunks 10  --prop $PROP '>' $OUTPUT
+ssh $SSH_HOST $INTRO '&&' cd $F '&&' DB=$TOSQL $PYTHON composite_properties.py --basegroup $BASE/groups_$SNAP/sub_$SNAP --basesnap $BASE/snapdir_$SNAP/snap_$SNAP --simulation-name $NAME   --snap $SNAP --chunk 0 --chunks 10  --prop $PROP #'>' $OUTPUT
 
 
 
