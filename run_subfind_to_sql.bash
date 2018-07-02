54d1a366122167869a2987fbeeb188cf5e387c3b
Antonio Ragagnin <ragagnin@dorc1.usm.uni-muenchen.de>
2018-07-02 23:44:52 +0200

    fix

--- run_subfind_to_sql.bash
+++ run_subfind_to_sql.bash
@@ -11,27 +11,27 @@ sqlite3 $DB '.index'
 
 echo NAME=$NAME LIM=$LIM   TAG=$TAG SNAP=$SNAP 
 
-#python subfind_to_sql.py  --basename $NAME/  --snap $SNAP --simulation-name $NAME --tag $TAG --min-val $LIM --add-fof 1
+python subfind_to_sql.py  --basename $NAME/  --snap $SNAP --simulation-name $NAME --tag $TAG --min-val $LIM --add-fof 1
 
-#SNAP_ID=$(sqlite3 $DB "
-#SELECT snap.id FROM snap 
-#inner join simulation on snap.simulation_id=simulation.id 
+SNAP_ID=$(sqlite3 $DB "
+SELECT snap.id FROM snap 
+inner join simulation on snap.simulation_id=simulation.id 
 #where simulation.name='$NAME' and snap.tag='$TAG'
-#")
+")
 
-#echo SNAP_ID=$SNAP_ID
+echo SNAP_ID=$SNAP_ID
 
-#sqlite3 $DB "
-#delete from fof 
-#where fof.snap_id='$SNAP_ID' AND  mcri<(
-#    select MAX(mcri) from fof 
-#    where  fof.snap_id='$SNAP_ID' AND glen<(
-#        select MIN(GLEN) from fof  
-#        where fof.snap_id='$SNAP_ID'
-#    )*1.1
-#)
-#"
+sqlite3 $DB "
+delete from fof 
+where fof.snap_id='$SNAP_ID' AND  mcri<(
+    select MAX(mcri) from fof 
+    where  fof.snap_id='$SNAP_ID' AND glen<(
+        select MIN(GLEN) from fof  
+        where fof.snap_id='$SNAP_ID'
+    )*1.1
+)
+"
 
-#python subfind_to_sql.py  --basename $NAME  --snap $SNAP --simulation-name $NAME --tag $TAG  --add-fof 0 --add-sf-bounds 1
+python subfind_to_sql.py  --basename $NAME  --snap $SNAP --simulation-name $NAME --tag $TAG  --add-fof 0 --add-sf-bounds 1
 
 python subfind_to_sql.py  --basename $NAME  --snap $SNAP --simulation-name $NAME --tag $TAG  --add-fof 0 --add-sf-bounds 0 --add-sf-data  1
