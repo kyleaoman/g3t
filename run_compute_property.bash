71db524b5f0b7977ae95740bf12e40aee923a00f
Antonio Ragagnin <ragagnin@dorc1.usm.uni-muenchen.de>
2018-07-02 21:04:32 +0200

    fix

--- run_compute_property.bash
+++ run_compute_property.bash
@@ -8,11 +8,11 @@ export TARGET=2018_jul_3
 #. sims.bash; Box0mr_bao_z2; db_1e5_22j; MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
 
 
-. sims.bash; Box2bhr_bao_z0; db_1e5_22j;TARGET=2018_jul PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
-#. sims.bash; Box2bhr_bao_z05; db_1e5_22j;TARGET=2018_jul PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
-#. sims.bash; Box2bhr_bao_z1; db_1e5_22j;TARGET=2018_jul PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
-#. sims.bash; Box2bhr_bao_z15; db_1e5_22j;TARGET=2018_jul PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
-#. sims.bash; Box2bhr_bao_z2; db_1e5_22j;TARGET=2018_jul PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
+. sims.bash; Box2bhr_bao_z0; db_1e5_22j;TARGET=$TARGET PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
+#. sims.bash; Box2bhr_bao_z05; db_1e5_22j;TARGET=$TARGET PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
+#. sims.bash; Box2bhr_bao_z1; db_1e5_22j;TARGET=$TARGET PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
+#. sims.bash; Box2bhr_bao_z15; db_1e5_22j;TARGET=$TARGET PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
+#. sims.bash; Box2bhr_bao_z2; db_1e5_22j;TARGET=$TARGET PROP=$PROP MEMO=$BOXNAME$TAG ./ssh_launch_job.bash
 
 
 
--- run_subfind_to_sql_all_boxes.bash
+++ run_subfind_to_sql_all_boxes.bash
@@ -5,17 +5,26 @@
 #. sims.bash; Box0mr_bao_z2; db_1e5_22j; ./run_subfind_to_sql.bash
 
 
+. sims.bash; Box0mr_dm_z0; db_1e5_22j; ./run_subfind_to_sql.bash
+. sims.bash; Box0mr_dm_z05; db_1e5_22j; ./run_subfind_to_sql.bah
+. sims.bash; Box0mr_dm_z1; db_1e5_22j; ./run_subfind_to_sql.bash
+. sims.bash; Box0mr_dm_z15; db_1e5_22j; ./run_subfind_to_sql.bash
+. sims.bash; Box0mr_dm_z2; db_1e5_22j; ./run_subfind_to_sql.bash
+
+
+
+
 #. sims.bash; Box2bhr_bao_z0; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box2bhr_bao_z05; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box2bhr_bao_z1; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box2bhr_bao_z15; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box2bhr_bao_z2; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box2bhr_bao_z05; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box2bhr_bao_z1; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box2bhr_bao_z15; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box2bhr_bao_z2; db_1e5_22j; ./run_subfind_to_sql.bash
 
 
 
 
-. sims.bash; Box4uhr_bao_z0; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box4uhr_bao_z05; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box4uhr_bao_z1; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box4uhr_bao_z15; db_1e5_22j; ./run_subfind_to_sql.bash
-. sims.bash; Box4uhr_bao_z2; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box4uhr_bao_z0; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box4uhr_bao_z05; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box4uhr_bao_z1; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box4uhr_bao_z15; db_1e5_22j; ./run_subfind_to_sql.bash
+#. sims.bash; Box4uhr_bao_z2; db_1e5_22j; ./run_subfind_to_sql.bash
